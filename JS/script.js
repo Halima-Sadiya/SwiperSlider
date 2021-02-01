79c09c494c11880fa5bac8457fb10777fa0546e1
codermim <codermim01@gmail.com>
2021-02-01 23:40:48 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+    effect: 'cube',
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+
+  pagination: {
+    el: '.swiper-pagination',
+      type: 'fraction',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
